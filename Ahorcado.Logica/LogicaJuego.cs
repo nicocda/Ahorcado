8f04707dafe1c7e3e03a010a9d798c37aa8aacb5
Nicolás Giordano <nicolas.giordano@ndvsistemas.com>
2020-09-22 20:49:24 -0300

    Acomodo la cagada que me mande

---
+++ Ahorcado.Logica/LogicaJuego.cs
@@ -0,0 +1,29 @@
+﻿using Ahorcado.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahorcado.Logica
+{
+    public class LogicaJuego
+    {
+        private Juego Juego;
+
+        public LogicaJuego()
+        {
+            Juego = new Dominio.Juego();
+        }
+
+        public void IngresarPalabra(string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ValidarPalabra()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Ahorcado.UnitTest/Ahorcado.cs
+++ Ahorcado.UnitTest/Ahorcado.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using Ahorcado.Dominio;
+using Ahorcado.Logica;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Ahorcado.UnitTest
@@ -11,7 +12,7 @@ namespace Ahorcado.UnitTest
         [TestMethod]
         public void Palabra_SinNumeros()
         {
-            Juego juego = new Juego();
+            Dominio.Juego juego = new Dominio.Juego();
 
             bool contieneUnNumero = !juego.Palabra.Any(char.IsDigit);
             Assert.IsTrue(contieneUnNumero);
@@ -20,7 +21,7 @@ namespace Ahorcado.UnitTest
         [TestMethod]
         public void Palabra_CantidadLetras8()
         {
-            Juego juego = new Juego();
+            Dominio.Juego juego = new Dominio.Juego();
 
             Assert.AreEqual(juego.Palabra.Length, 8);
         }
@@ -28,7 +29,7 @@ namespace Ahorcado.UnitTest
         [TestMethod]
         public void Palabra_SinSimbolosRaros()
         {
-            Juego juego = new Juego();
+            Dominio.Juego juego = new Dominio.Juego();
 
             bool todasLetras = juego.Palabra.All(char.IsLetter);
             Assert.IsTrue(todasLetras);
@@ -37,7 +38,7 @@ namespace Ahorcado.UnitTest
         [TestMethod]
         public void Palabra_SinEspacios()
         {
-            Juego juego = new Juego();
+            Dominio.Juego juego = new Dominio.Juego();
 
             bool conieneEspacios = juego.Palabra.Any(c=> c.Equals(" "));
             Assert.IsFalse(conieneEspacios);
@@ -48,7 +49,7 @@ namespace Ahorcado.UnitTest
         [TestMethod]
         public void Usuario_NoNull()
         {
-            Juego juego = new Juego();
+            Dominio.Juego juego = new Dominio.Juego();
 
             Assert.IsNotNull(juego.Usuario);
         }
@@ -56,14 +57,14 @@ namespace Ahorcado.UnitTest
         [TestMethod]
         public void Usuario_NoVacio()
         {
-            Juego juego = new Juego();
+            Dominio.Juego juego = new Dominio.Juego();
             Assert.IsFalse(string.IsNullOrEmpty(juego.Usuario));
         }
 
         [TestMethod]
         public void Probar_Palabra_IngresadaIsLetter()
         {
-            Juego juego = new Juego();
+            Dominio.Juego juego = new Dominio.Juego();
             juego.PalabraIngresada = "PalabraPorTeclado";
             bool todasLetras = juego.PalabraIngresada.All(char.IsLetter);
             Assert.IsTrue(todasLetras);
@@ -72,7 +73,7 @@ namespace Ahorcado.UnitTest
         [TestMethod]
         public void PalbraIngresada_hasNotSpace()
         {
-            Juego juego = new Juego();
+            Dominio.Juego juego = new Dominio.Juego();
             juego.PalabraIngresada = "PalabraPorTeclado";
             bool todasLetras = juego.PalabraIngresada.Contains(" ");
             Assert.IsFalse(todasLetras);
@@ -82,11 +83,23 @@ namespace Ahorcado.UnitTest
         [TestMethod]
         public void Probar_Palabra_Ingresada()
         {
-            Logica logica = new Logica();
+            LogicaJuego logica = new LogicaJuego();
             logica.IngresarPalabra("asadwerá");
             bool palabraEsIgual = logica.ValidarPalabra();
             Assert.IsTrue(palabraEsIgual);
         }
 
+
+
+
+
+        [TestMethod]
+        public void Probar_Palabra_Ingresada_Sea_Igual()
+        {
+            var logica = new LogicaJuego();
+            logica.IngresarPalabra("asadwerá");
+            bool palabraEsIgual = logica.ValidarPalabra();
+            Assert.IsTrue(palabraEsIgual);
+        }
     }
 }
