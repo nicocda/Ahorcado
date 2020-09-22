eaa5f7ef03d2f4c674fc34c1015efbd819213be3
Nicolás Giordano <nicolas.giordano@ndvsistemas.com>
2020-09-22 20:34:14 -0300

    Modifico Nombre de Usuario

--- Ahorcado.Dominio/Juego.cs
+++ Ahorcado.Dominio/Juego.cs
@@ -9,6 +9,6 @@ namespace Ahorcado.Dominio
     public class Juego
     {
         public string Palabra = "asadwerá";
-        public string Usuario = "asd ";
+        public string Usuario = "John Cena";
     }
 }
