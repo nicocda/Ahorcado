9b488f088fa53533f6810b42902294fde0342901
Nicolás Giordano <nicolas.giordano@ndvsistemas.com>
2020-09-22 20:45:14 -0300

    Agrego clase de Logica

---
+++ Ahorcado.Logica/Logica.cs
@@ -0,0 +1,21 @@
+﻿using Ahorcado.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahorcado.Logica
+{
+    public class Logica
+    {
+        private Juego Juego;
+
+        public Logica()
+        {
+            Juego = new Juego();
+        }
+
+        
+    }
+}
