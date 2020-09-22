3277c5bd99479b1af7fcc135382572372775ff88
Nicolás Giordano <nicolas.giordano@ndvsistemas.com>
2020-09-22 20:00:54 -0300

    Agrego Clase Juego y Palabra hardcodeada

---
+++ Ahorcado.Dominio/Juego.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahorcado.Dominio
+{
+    public class Juego
+    {
+        public string Palabra = "Palabra8";
+    }
+}
