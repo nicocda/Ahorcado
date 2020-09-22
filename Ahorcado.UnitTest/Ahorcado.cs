c46188f0e4b03ed1dae4c40f7799479ea7274569
Nicolás Giordano <nicolas.giordano@ndvsistemas.com>
2020-09-22 20:06:42 -0300

    Agrego Primer Test palabra de 8 caracteres

---
+++ Ahorcado.UnitTest/Ahorcado.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Ahorcado.Dominio;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ahorcado.UnitTest
+{
+    [TestClass]
+    public class Ahorcado
+    {
+        [TestMethod]
+        public void Palabra_CantidadLetras8()
+        {
+            Juego juego = new Juego();
+
+            Assert.AreEqual(juego.Palabra.Length, 8);
+        }
+    }
+}
