4099473a349d2ac4baa86aef70aa2715537dbb76
Nicolás Giordano <nicolas.giordano@ndvsistemas.com>
2020-10-26 19:42:25 -0300

    configuraciones basicas MVC

--- Ahorcado.MVC/Controllers/AhorcadoController.cs
+++ Ahorcado.MVC/Controllers/AhorcadoController.cs
@@ -1,10 +1,7 @@
-﻿using Ahorcado.MVC.Models;
-using System;
+﻿using Ahorcado.Logica;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
-using Ahorcado.Logica;
 
 namespace Ahorcado.MVC.Controllers
 {
